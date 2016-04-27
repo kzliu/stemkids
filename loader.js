b44f9771db2e0902640ba6518adcff62175761fa
Gregory Cho <gregwbcho@gmail.com>
2016-04-27 00:56:50 -0400

    updated loader

--- loader.js
+++ loader.js
@@ -6,10 +6,10 @@ var conn = anyDB.createConnection('sqlite3://stemkids.sqlite3');
 conn.query('CREATE TABLE IF NOT EXISTS user_info (id INTEGER PRIMARY KEY AUTOINCREMENT, login TEXT, first_name TEXT, last_name TEXT, grade TEXT, school TEXT, gender TEXT, email TEXT, phone_num INTEGER password TEXT);').on('error', console.error).on('end', function(){
 	console.log('user_info table successfully created');
 });
-// create course class (multiple courses in a class) table
-conn.query('CREATE TABLE IF NOT EXISTS course_classes (course_id INTEGER, course_class TEXT, course_order INTEGER, PRIMARY KEY (course_id));')
+// create class to course (multiple classes in a course) mapping
+conn.query('CREATE TABLE IF NOT EXISTS course_classes (class_id INTEGER, course TEXT, class_order INTEGER, PRIMARY KEY (course_id));')
 // create courses table
-conn.query('CREATE TABLE IF NOT EXISTS courses (course_id INTEGER PRIMARY KEY AUTOINCREMENT, course_description TEXT, instructor TEXT, PRIMARY KEY (course_id), FOREIGN KEY (course_id) REFERENCES course_classes);').on('error', console.error).on('end', function(){
+conn.query('CREATE TABLE IF NOT EXISTS classes (class_id INTEGER PRIMARY KEY AUTOINCREMENT, class_description TEXT, instructor TEXT, PRIMARY KEY (class_id), FOREIGN KEY (class_id) REFERENCES course_classes);').on('error', console.error).on('end', function(){
 	console.log('courses table successfully created');
 });
 // create course information table
@@ -17,4 +17,4 @@ conn.query('CREATE TABLE IF NOT EXISTS course_attendance (user_id INTEGER, cours
 	console.log('course_info table successfully created');
 });
 // create course progress table
-conn.query('CREATE TABLE IF NOT EXISTS course_progress (user_id INTEGER, course_class TEXT, progress INTEGER)')
+conn.query('CREATE TABLE IF NOT EXISTS class_progress (user_id INTEGER, course TEXT, progress INTEGER)')
--- server.js
+++ server.js
@@ -16,11 +16,18 @@ app.get('/profile/:identifyer', function(request, response){
 	var q = conn.query("SELECT * FROM user_info, course, course_attendance WHERE user_info.id = $1", [identifyer], function(err, data){
 		// send data to the front end as a response
 		response.json(data.rows);
+		// print response to console (should be unique)
 		console.log(data.rows);
 		// finish and close response
 		response.end();
 	});
+	// print to console upon successful query completion
 	q.on('end', function(){
 		console.log('data successfuly sent')
 	});
+});
+
+// set the app's server to listen on a given port
+app.listen(1234, function(){
+	console.log('- Server listening on given port');
 });
